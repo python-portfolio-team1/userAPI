968f30c4a2879d24a2f8993e7b0e3ac3c276386a
Dumebiokafor <dumebiokafor20@gmail.com>
2022-06-10 20:29:11 +0100

    adjusted url

--- user_api/serializers.py
+++ user_api/serializers.py
@@ -2,7 +2,7 @@ from pyexpat import model
 from rest_framework import serializers
 from .models import Users
 
-class UsersSerializer(serializers.modelserializer):
+class UsersSerializer(serializers.ModelSerializer):
     class Meta:
         model = Users
         fields = ['id', 'firstname', 'lastname', 'email', 'birthday', 'phone', 'address', 'password']
